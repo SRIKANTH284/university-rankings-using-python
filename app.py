5ef9205e7e954ce88ac6fe7fa3d12d5e84bd6a89
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2023-07-24 14:31:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,50 @@ def get_university_data():
 
 df = get_university_data()
 
+# Function to filter data based on user selection
+def filter_data(df, country, year):
+    df_selection = df.query("country == @country & year == @year")
+    return df_selection
+
+# Function to create university distribution bar chart
+def create_university_distribution_chart(df_selection):
+    university_distribution = df_selection["country"].value_counts().reset_index()
+    university_distribution.columns = ["Country", "Number of Universities"]
+    
+    fig_university_distribution = px.bar(
+        university_distribution,
+        x="Number of Universities",
+        y="Country",
+        orientation="h",
+        title="<b>University Distribution by Country</b>",
+        color_discrete_sequence=["#0083B8"] * len(university_distribution),
+        template="plotly_white",
+    )
+    fig_university_distribution.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=dict(showgrid=False),
+    )
+    
+    return fig_university_distribution
+
+# Function to create university ranking trend line chart
+def create_ranking_trend_chart(df_selection):
+    fig_ranking_trend = px.line(
+        df_selection,
+        x="year",
+        y="world_rank",
+        color="institution",
+        title="<b>University Ranking Trend</b>",
+        template="plotly_white",
+    )
+    fig_ranking_trend.update_layout(
+        plot_bgcolor="rgba(0,0,0,0)",
+        xaxis=dict(showgrid=False),
+        yaxis=dict(showgrid=False),
+    )
+    
+    return fig_ranking_trend
+
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
 country = st.sidebar.multiselect(
@@ -24,9 +68,9 @@ country = st.sidebar.multiselect(
     default=df["country"].unique()
 )
 
-year_min = int(df["year"].min())  # Convert min_value to integer
-year_max = int(df["year"].max())  # Convert max_value to integer
-year_value = year_max  # Set initial value to max year
+year_min = df["year"].min()
+year_max = df["year"].max()
+year_value = year_max
 
 year = st.sidebar.slider(
     "Select Year:",
@@ -35,7 +79,8 @@ year = st.sidebar.slider(
     value=year_value,
 )
 
-df_selection = df.query("country == @country & year == @year")
+# Filter data based on user selection
+df_selection = filter_data(df, country, year)
 
 # ---- MAIN PAGE ----
 st.title(":mortar_board: University Rankings")
@@ -49,40 +94,11 @@ st.dataframe(top_10_universities)
 st.markdown("""---""")
 
 # UNIVERSITY DISTRIBUTION BY COUNTRY [BAR CHART]
-university_distribution = df_selection["country"].value_counts().reset_index()
-university_distribution.columns = ["Country", "Number of Universities"]
-
-fig_university_distribution = px.bar(
-    university_distribution,
-    x="Number of Universities",
-    y="Country",
-    orientation="h",
-    title="<b>University Distribution by Country</b>",
-    color_discrete_sequence=["#0083B8"] * len(university_distribution),
-    template="plotly_white",
-)
-fig_university_distribution.update_layout(
-    plot_bgcolor="rgba(0,0,0,0)",
-    xaxis=dict(showgrid=False),
-)
-
+fig_university_distribution = create_university_distribution_chart(df_selection)
 st.plotly_chart(fig_university_distribution, use_container_width=True)
 
 # UNIVERSITY RANKING TREND [LINE CHART]
-fig_ranking_trend = px.line(
-    df_selection,
-    x="year",
-    y="world_rank",
-    color="institution",
-    title="<b>University Ranking Trend</b>",
-    template="plotly_white",
-)
-fig_ranking_trend.update_layout(
-    plot_bgcolor="rgba(0,0,0,0)",
-    xaxis=dict(showgrid=False),
-    yaxis=dict(showgrid=False),
-)
-
+fig_ranking_trend = create_ranking_trend_chart(df_selection)
 st.plotly_chart(fig_ranking_trend, use_container_width=True)
 
 # ---- HIDE STREAMLIT STYLE ----
